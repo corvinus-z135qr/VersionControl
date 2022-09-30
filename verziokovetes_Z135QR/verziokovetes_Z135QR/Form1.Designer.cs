9f5fd8d0b63223f20c9707a34f116773324596d6
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-09-30 21:38:12 +0200

    Fájlba írás

--- verziokovetes_Z135QR/verziokovetes_Z135QR/Form1.Designer.cs
+++ verziokovetes_Z135QR/verziokovetes_Z135QR/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.lblLastName = new System.Windows.Forms.TextBox();
             this.lblFirstName = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.btn_fajlbairas = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -86,11 +87,21 @@
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // btn_fajlbairas
+            // 
+            this.btn_fajlbairas.Location = new System.Drawing.Point(278, 137);
+            this.btn_fajlbairas.Name = "btn_fajlbairas";
+            this.btn_fajlbairas.Size = new System.Drawing.Size(214, 31);
+            this.btn_fajlbairas.TabIndex = 6;
+            this.btn_fajlbairas.Text = "Fájlba írás";
+            this.btn_fajlbairas.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_fajlbairas);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lblFirstName);
             this.Controls.Add(this.lblLastName);
@@ -112,6 +123,7 @@
         private System.Windows.Forms.TextBox lblLastName;
         private System.Windows.Forms.TextBox lblFirstName;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btn_fajlbairas;
     }
 }
 
