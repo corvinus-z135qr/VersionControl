650a89f93bd25b57dd85a6d90b26a55df8d223b0
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-10-06 12:36:13 +0200

    04_ Adatok betöltése

--- 04_excel_Z135QR/04_excel_Z135QR/Form1.cs
+++ 04_excel_Z135QR/04_excel_Z135QR/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,17 @@ namespace _04_excel_Z135QR
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities(); 
+        List<Flat> Flats; 
         public Form1()
         {
+            
             InitializeComponent();
+
+        }
+        private void LoadData()
+        {
+            List<Flat> Flats = context.Flat.ToList(); //problemas
         }
     }
 }
