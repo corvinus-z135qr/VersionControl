066df3d0ffb239fd3a564fd43e5e7fbdcde92790
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-10-06 12:52:55 +0200

    06_ Excel munkafuzet letrehozasa es megnyitasa (void tipusu fuggveny, CreateTable meg nem keszult el-kommentelve van)

--- 04_excel_Z135QR/04_excel_Z135QR/Form1.cs
+++ 04_excel_Z135QR/04_excel_Z135QR/Form1.cs
@@ -16,7 +16,12 @@ namespace _04_excel_Z135QR
     public partial class Form1 : Form
     {
         RealEstateEntities context = new RealEstateEntities(); 
-        List<Flat> Flats; 
+        List<Flat> Flats;
+
+        //06_os feladat (excel munkafuzet letrehozasa/megnyitasa)
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             
@@ -26,6 +31,31 @@ namespace _04_excel_Z135QR
         private void LoadData()
         {
             List<Flat> Flats = context.Flat.ToList(); //problemas
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
