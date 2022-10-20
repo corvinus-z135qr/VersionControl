19bbdd7030ca21e32ddd12d4c4159e79f12e02ea
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-10-20 20:23:34 +0200

    02 ) Webszolgaltatas hozzaadasa a projekthez 03) Webszolgaltatas hivasa

--- webszolgaltatas_05/webszolgaltatas_05/Form1.cs
+++ webszolgaltatas_05/webszolgaltatas_05/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using webszolgaltatas_05.MnbServiceReference;
 
 namespace webszolgaltatas_05
 {
     public partial class Form1 : Form
     {
+       
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
+        
     }
 }
