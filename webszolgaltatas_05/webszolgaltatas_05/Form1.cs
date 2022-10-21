6974230cd835bb2127dc9f8bea9ede4dcbee7f8e
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-10-21 08:40:28 +0200

    07) Dinamikus funkciók

--- webszolgaltatas_05/webszolgaltatas_05/Form1.cs
+++ webszolgaltatas_05/webszolgaltatas_05/Form1.cs
@@ -80,6 +80,39 @@ namespace webszolgaltatas_05
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
         }
+        void RefreshData()
+        {
+            chartRateData.DataSource = Rates;
 
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+            //List.Clear.RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
