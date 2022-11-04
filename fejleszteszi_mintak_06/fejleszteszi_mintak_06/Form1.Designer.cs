58f0428acbd117ef012abacc9e012efa27c9667e
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-11-04 18:49:22 +0100

    11) Termékválasztó gombok

--- fejleszteszi_mintak_06/fejleszteszi_mintak_06/Form1.Designer.cs
+++ fejleszteszi_mintak_06/fejleszteszi_mintak_06/Form1.Designer.cs
@@ -31,16 +31,52 @@ namespace fejleszteszi_mintak_06
         {
             this.components = new System.ComponentModel.Container();
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_ball = new System.Windows.Forms.Button();
+            this.button_car = new System.Windows.Forms.Button();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.button_ball);
+            this.mainPanel.Controls.Add(this.button_car);
             this.mainPanel.Location = new System.Drawing.Point(1, -1);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(870, 532);
             this.mainPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(336, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ComingNext: ";
+            // 
+            // button_ball
+            // 
+            this.button_ball.Location = new System.Drawing.Point(112, 58);
+            this.button_ball.Name = "button_ball";
+            this.button_ball.Size = new System.Drawing.Size(75, 23);
+            this.button_ball.TabIndex = 1;
+            this.button_ball.Text = "Ball";
+            this.button_ball.UseVisualStyleBackColor = true;
+            this.button_ball.Click += new System.EventHandler(this.button_ball_Click);
+            // 
+            // button_car
+            // 
+            this.button_car.Location = new System.Drawing.Point(11, 58);
+            this.button_car.Name = "button_car";
+            this.button_car.Size = new System.Drawing.Size(75, 23);
+            this.button_car.TabIndex = 0;
+            this.button_car.Text = "Car";
+            this.button_car.UseVisualStyleBackColor = true;
+            this.button_car.Click += new System.EventHandler(this.button_car_Click);
             // 
             // createTimer
             // 
@@ -62,6 +98,8 @@ namespace fejleszteszi_mintak_06
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +109,9 @@ namespace fejleszteszi_mintak_06
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_ball;
+        private System.Windows.Forms.Button button_car;
     }
 }
 
--- fejleszteszi_mintak_06/fejleszteszi_mintak_06/Form1.cs
+++ fejleszteszi_mintak_06/fejleszteszi_mintak_06/Form1.cs
@@ -15,13 +15,29 @@ namespace fejleszteszi_mintak_06
     public partial class Form1 : Form
     {
         private List<Toy> _toys = new List<Toy>();
-
-        private BallFactory _factory;
+        private Toy _nextToy;
+        private IToyFactory _factory;
+        //private BallFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = (BallFactory)value; }
+            set {
+                _factory = value;
+                DisplayNext();
+
+            }
         }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -52,6 +68,16 @@ namespace fejleszteszi_mintak_06
             _toys.Add(toy);
             toy.Left = -toy.Width;
             mainPanel.Controls.Add(toy);
+        }
+
+        private void button_car_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button_ball_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
         }
     }
 }
