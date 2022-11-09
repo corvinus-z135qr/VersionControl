5f5681d4059ed7160a6426d4e9956251fcefc38e
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-11-09 14:05:03 +0100

    04) Adatok betöltése

--- Var_07/Var_07/Form1.cs
+++ Var_07/Var_07/Form1.cs
@@ -12,9 +12,13 @@ namespace Var_07
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
