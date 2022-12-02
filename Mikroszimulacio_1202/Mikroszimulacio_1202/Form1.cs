18a80609fb3a1d59e7ca36e38d8fe6c1b237db80
Alex Bodnár <alex.bodnar2@stud.uni-corvinus.hu>
2022-12-02 11:21:00 +0100

    06) Szimulacios lepes

--- Mikroszimulacio_1202/Mikroszimulacio_1202/Form1.cs
+++ Mikroszimulacio_1202/Mikroszimulacio_1202/Form1.cs
@@ -30,7 +30,7 @@ namespace Mikroszimulacio_1202
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // szimulacios lepes
+                    SimStep(year, Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -112,5 +112,35 @@ namespace Mikroszimulacio_1202
 
             return dp;
         }
+
+      
+            public void SimStep(int year, Person person)
+            {
+                if (!person.IsAlive) return;
+
+                byte age = (byte)(year - person.BirthYear);
+
+                double pDeath = (from x in DeathProbabilities
+                                 where x.Gender == person.Gender && x.Age == age
+                                 select x.DyingProb).FirstOrDefault();
+                if (rng.NextDouble() <= pDeath)
+                    person.IsAlive = false;
+
+                if (person.IsAlive && person.Gender == Gender.Female)
+                {
+                    double pBirth = (from x in BirthProbabilities
+                                     where x.Age == age
+                                     select x.BirthGivenProb).FirstOrDefault();
+                    if (rng.NextDouble() <= pBirth)
+                    {
+                        Person újszülött = new Person();
+                        újszülött.BirthYear = year;
+                        újszülött.NbrOfChildren = 0;
+                        újszülött.Gender = (Gender)(rng.Next(1, 3));
+                        Population.Add(újszülött);
+                    }
+                }
+            }
+        }
     }
-}
+
